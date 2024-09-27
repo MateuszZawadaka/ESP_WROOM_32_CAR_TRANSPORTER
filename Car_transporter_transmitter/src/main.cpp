06a55b8b7c946397f94c6d54ee84ae5509954850
Mateusz Zawadka <mateusz.zawadka@atmat.pl>
2024-09-27 11:44:04 +0200

    Solved the bug in car transporter transmitter

--- Car_transporter_transmitter/src/main.cpp
+++ Car_transporter_transmitter/src/main.cpp
@@ -1,131 +1,16 @@
-//-------------------------------------------------------------------------//
-//------- Code for car transporter transmitter ----------------------------//
-//-------------------------------------------------------------------------// 
+
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
 #include <pinsModeSetup.h>
-// // put function declarations here:
-// int myFunction(int, int);
-// // void setInputMode(int);
-// void testOutputsHigh(int);
-// void testOutputsLow(int);
+
 // void readMacAddress();
-// void OnDataSent(const uint8_t , esp_now_send_status_t);
-// const int switchesPin[] = {32, 33, 25, 26, 27, 14, 12, 23, 22, 21, 19, 18, 5, 17};
-// const int statusLed = 16;
-// uint8_t receiverMacAddress[] = {0x30, 0xc9, 0x22, 0x27, 0xe8, 0x28};
-// typedef struct TransmitterStruct
-// {
-//   bool switchesAction[14];
 
-// }TransmitterStruct;
-// TransmitterStruct sentData;
-// pinsModeSetup inputs;
-// void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-// {
-//   Serial.print("\r\nLast Packet Send Status:\t");
-//   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-// }
-// void setup() {
-//   inputs.setInputMode(14, switchesPin);
-//   Serial.begin(9600);
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin();
-//   Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
-//   readMacAddress();
-//   int result = myFunction(2, 3);
-
-//   if(esp_now_init() != ESP_OK){
-//     Serial.println("Error initializing ESP-NOW");
-//     return;
-
-//   }
-//   esp_now_register_send_cb(OnDataSent);
-
-//   esp_now_peer_info_t peerInfo;
-//   memcpy(peerInfo.peer_addr, receiverMacAddress, 6);
-//   peerInfo.channel = 0;  
-//   peerInfo.encrypt = false;
-//   if(esp_now_add_peer(&peerInfo) != ESP_OK)
-//   {
-//     Serial.println("Failed to add peer");
-//     return;
-//   }
-  
-// }
-
-// void loop() {
-//   for (int i = 0; i < 14; i++)
-//   {
-//     delayMicroseconds(30);
-//     sentData.switchesAction[i] = digitalRead(switchesPin[i]);
-    
-//   }
-  
-//   //-------------------------------------------------------------------------------------
-//   esp_err_t result = esp_now_send(receiverMacAddress, (uint8_t *) &sentData, sizeof(sentData));
-//   //-------------------------------------------------------------------------------------
-//   if (result == ESP_OK) Serial.println("Sent with success");
-//   else Serial.println("Error sending the data");
-//   //-------------------------------------------------------------------------------------
-//   delay(100);
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
-// // void setInputMode(int pin){
-// //   pinMode(pin, INPUT_PULLUP);
-
-// // }
-
-// // void testOutputsHigh(int output){
-// //   digitalWrite(output, HIGH);
-// //   Serial.println(digitalRead(output));
-// //   delayMicroseconds(100);
-// // }
-
-// // void testOutputsLow(int output){
-// //   digitalWrite(output, LOW);
-// //   Serial.println(digitalRead(output));
-// //   delayMicroseconds(100);
-// // }
-
-// void readMacAddress(){
-//   uint8_t baseMac[6];
-//   esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
-
-//   if (ret == ESP_OK)
-//   {
-//     Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
-//                   baseMac[0], baseMac[1], baseMac[2], baseMac[3],
-//                   baseMac[4], baseMac[5]);
-//   } else{
-//     Serial.println("Failed to read MAC address");
-//   }
-  
-// 
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-//------------------------------------------------------------------------------------------------------------//
-
-
-// Variables for test data
 int int_value;
 float float_value;
 bool bool_value = true;
-
+bool bMacAddressReaded = false;
 // MAC Address of responder - edit as required
 uint8_t broadcastAddress[] = {0x30, 0xc9, 0x22, 0x27, 0xe8, 0x28};
 
@@ -145,6 +30,7 @@ struct_message myData;
 esp_now_peer_info_t peerInfo;
 const int switchesPin[] = {32, 33, 25, 26, 27, 14, 12, 23, 22, 21, 19, 18, 5, 17};
 // Callback function called when data is sent
+void readMacAddressFromUser();
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
@@ -158,7 +44,11 @@ void setup() {
   {
     pinMode(switchesPin[i], INPUT_PULLUP);
   }
-  
+  if(bMacAddressReaded == false)
+  {
+    readMacAddressFromUser();
+  }
+
   // Set ESP32 as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
@@ -168,6 +58,7 @@ void setup() {
     return;
   }
 
+  
   // Register the send callback
   esp_now_register_send_cb(OnDataSent);
   
@@ -216,5 +107,67 @@ void loop() {
   else {
     Serial.println("Sending error");
   }
-  delay(2000);
+  delay(200);
+}
+
+// void readMacAddress(){
+//   esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, broadcastAddress);
+//   if (ret == ESP_OK) {
+//     Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+//                   broadcastAddress[0], broadcastAddress[1], broadcastAddress[2],
+//                   broadcastAddress[3], broadcastAddress[4], broadcastAddress[5]);
+//   bMacAddressReaded = true;
+//   } else {
+//     Serial.println("Failed to read MAC address");
+//     bMacAddressReaded = false;
+//   }
+// }
+void readMacAddressFromUser(){
+
+    Serial.println("Write first recieiver MAC char example: 0xc9");
+    broadcastAddress[0] = Serial.parseInt();
+    while (Serial.available() == 0)
+    {
+      /* code */
+    }
+    
+    Serial.println("Write second recieiver MAC char example: 0xc9");
+    broadcastAddress[1] = Serial.parseInt();
+    while (Serial.available() == 0)
+    {
+      /* code */
+    }
+    Serial.println("Write third recieiver MAC char example: 0xc9");
+    broadcastAddress[2] = Serial.parseInt();
+    while (Serial.available() == 0)
+    {
+      /* code */
+    }
+    Serial.println("Write fourth recieiver MAC char example: 0xc9");
+    broadcastAddress[3] = Serial.parseInt();
+    while (Serial.available() == 0)
+    {
+      /* code */
+    }
+    Serial.println("Write fifth recieiver MAC char example: 0xc9");
+    broadcastAddress[4] = Serial.parseInt();
+    while (Serial.available() == 0)
+    {
+      /* code */
+    }
+    Serial.println("Write sixth recieiver MAC char example: 0xc9");
+    broadcastAddress[5] = Serial.parseInt();
+    while (Serial.available() == 0)
+    {
+      /* code */
+    }
+    
+    Serial.print(broadcastAddress[0]);
+    Serial.print(broadcastAddress[1]);
+    Serial.print(broadcastAddress[2]);
+    Serial.print(broadcastAddress[3]);
+    Serial.print(broadcastAddress[4]);
+    Serial.print(broadcastAddress[5]);
+
+    bMacAddressReaded = true;
 }
